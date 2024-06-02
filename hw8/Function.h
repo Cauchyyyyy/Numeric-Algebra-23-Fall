6787b2f9cec32e5db7cb103c1c7f70e39aef8f1a
Cauchyyyyy <124767414+Cauchyyyyy@users.noreply.github.com>
2024-06-02 17:21:42 +0800

    modified:   hw8/Function.h

--- hw8/Function.h
+++ hw8/Function.h
@@ -3,54 +3,57 @@
 #include<vector>
 using namespace std;
 
-void matrix_transpose(vector<vector<double>>& A);//三角矩阵转置
+void matrix_transpose(vector<vector<double>>& A);//transpose of triangular matrix
 
-void forward_subs(vector<vector<double>>& L, vector<double>& b);//前代法
+void forward_subs(vector<vector<double>>& L, vector<double>& b);//forward substitution
 
-void forward_subs1(vector<vector<double>>& L, vector<double>& b);//对角元为1的前代法
+void forward_subs1(vector<vector<double>>& L, vector<double>& b);//forward substitution for diagonal elements are 1
 
-void back_subs(vector<vector<double>>& U, vector<double>& b);//回代法
+void back_subs(vector<vector<double>>& U, vector<double>& b);//back substitution
 
-void back_subs1(vector<vector<double>>& U, vector<double>& y);//对角元为1的回代法
+void back_subs1(vector<vector<double>>& U, vector<double>& y);//back substitution for diagonal elements are 1
 
-void gauss_elim(vector<vector<double>>& A);//Gauss消去法
+void gauss_elim(vector<vector<double>>& A);//Gauss elimination
 
-void gauss_elim_full_pivoting(vector<vector<double>>& A, vector<int>& u, vector<int>& v);//全主元Gauss消去法
+void gauss_elim_full_pivoting(vector<vector<double>>& A, vector<int>& u, vector<int>& v);//full pivoting Gauss elimination
 
-void gauss_elim_col_pivoting( vector<vector<double>>& A, vector<int>& u);//列主元Gauss消去法
+void gauss_elim_col_pivoting( vector<vector<double>>& A, vector<int>& u);//column pivoting Gauss elimination
 
-void vector_pb(vector<int>&u,vector<double>&b);//计算向量P*b【可选】
+void vector_pb(vector<int>&u,vector<double>&b);//compute vector P*b
 
-void vector_qb(vector<int>& v, vector<double>& b);//计算向量Q*b【可选】
+void vector_qb(vector<int>& v, vector<double>& b);//compute vector Q*b
 
-void cholesky_decomp(vector<vector<double>>& A);//对称正定阵标准Cholesky分解
+void cholesky_decomp(vector<vector<double>>& A);//Cholesky decomposition
 
-void modified_cholesky_decomp(vector<vector<double>>& A);//改进的平方根法
+void modified_cholesky_decomp(vector<vector<double>>& A);//modified Cholesky decomposition
 
-void matrix_DLT(vector<vector<double>>& A);//计算矩阵D*L^T【可选】
+void matrix_DLT(vector<vector<double>>& A);//compute D*L^T
 
-void solution_print(vector<double>& b);//输出解
+void solution_print(vector<double>& b);//print the solution
 
-void matrix_print(vector<vector<double>>& A);//输出矩阵
+void matrix_print(vector<vector<double>>& A);//print the matrix
 
 void matrix_vector_multiply(vector<vector<double>>& A, vector<double>& b);
-//计算向量A*b,存储于b
+//compute the product of matrix A and vector b
+//store the result in b
 
-void matrix_full_transpose(vector<vector<double>>& A);//一般方阵转置
+void matrix_full_transpose(vector<vector<double>>& A);//transpose
 
-double vectors_multiply(vector<double>& a, vector<double>& b);//向量a,b内积
+double vectors_multiply(vector<double>& a, vector<double>& b);
+//compute the product of two vectors
+//return a*.b (inner product a.k.a dot product)
 
-double vector_infinity_norm(vector<double>& a);//向量a的无穷范数
+double vector_infinity_norm(vector<double>& a);//infinity norm of vector a
 
-double vector_1_norm(vector<double>& a);//向量a的1范数
+double vector_1_norm(vector<double>& a);//1 norm of vector a
 
-double vector_2_norm(vector<double>& a);//向量a的2范数
+double vector_2_norm(vector<double>& a);//2 norm of vector a
 
 double matrix_1_norm(vector<vector<double>>& B);//优化法估计矩阵的1范数
 
 double matrix_1_norm_traditional(vector<vector<double>>& B);//一般方法计算矩阵的1范数
 
-double matrix_infinity_norm(vector<vector<double>>& A);//计算矩阵的无穷范数
+double matrix_infinity_norm(vector<vector<double>>& A);//infinity norm of matrix A
 
 double matrix_inverse_infinity_norm(vector<vector<double>>& A);//估计矩阵A^(-1)的无穷范数
 
@@ -60,41 +63,42 @@ void qr_decomposition(vector<vector<double>>& A, vector<double>& b, vector<doubl
 //矩阵A的QR分解,计算结果为A的上三角为R，下三角是每次householder变换v的值，d存储v[0]
 
 void matrix_multiply(vector<vector<double>>& A, vector<vector<double>>& B);
-//计算矩阵乘积A*B，其中A是m*m的，B是m*n的，结果存储于B
+//A \in R^{m*m}, B \in R^{m*n}, B = A*B
 
 void matrix_vector_times(vector<vector<double>>& A, vector<double>& b, vector<double>& Ab);
-//计算A*b，A是m*n的，b是n*1的，存储于Ab中
+//A \in R^{m*n}, b \in R^{n*1}, Ab \in R^{m*1}
 
 void matrix_mn_transpose(vector<vector<double>>& A, vector<vector<double>>& AT);
-//一般矩阵的转置，存储于AT中
+//AT=A^T
 
 void qr_decomposition2(vector<vector<double>>& A, vector<double>& b, vector<double>& d);
 //矩阵A的QR分解,计算结果为A的上三角为R，下三角是每次householder变换v的值，d存储v[0]
 //不采用矩阵乘积
 
 void matrix_minus(vector<vector<double>>& A, vector<vector<double>>& B);
-//A-B存储于A
+//A=A-B
 
 void vector_vvT(vector<double>& b, vector<double>& bt, vector<vector<double>>& A);
-//v*v^T存储于A
+//A=b*b^T
 
 void vector_minus(vector<double>& b, vector<double>& c);
 //b=b-c
 
 double matrix_min(vector<vector<double>>& A);
-//矩阵元素的最小分量
+//minimum element of matrix A
 
 int conjugate_gradient(vector<vector<double>>& A, vector<double>& b);
 //实用共轭梯度法求解Ax=b,解存储于b,返回迭代次数
 
 int jacobi(vector<vector<double>>& A, vector<double>& b);
-//Jacobi迭代法，解存储于b，返回迭代次数
+//Jacobi iterative method, return the number of iteration times
+//solution stored in b
 
 int g_s(vector<vector<double>>& A, vector<double>& b);
-//G-S迭代法
+//Gauss-Seidel iterative method, return the number of iteration times
 
 int sor(vector<vector<double>>& A, vector<double>& b, double omega);
-//SOR迭代法
+//Successive Over Relaxation iterative method, return the number of iteration times
 
 double power_method(vector<vector<double>>& A, vector<double>& u);
 //幂法求特征值最大模,u是迭代的初始值
